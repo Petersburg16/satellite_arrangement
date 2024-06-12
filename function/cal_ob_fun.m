bf39b57b130e2d916606b07e14d3cb47ed7ffa5b
WeiMing <weiming1116@outlook.com>
2024-06-12 13:44:44 +0800

    修改目标函数计算方式为临近的前后卫星

--- function/cal_ob_fun.m
+++ function/cal_ob_fun.m
@@ -4,17 +4,18 @@ function ob_fun=cal_ob_fun(x,sat_array,sat_num,k)
 % sat_num:对该组卫星中的哪科进行优化
 % k：目标函数中的gama
 
+A=cal_matrix_A(sat_array);
 
 % 累加邻近卫星的时间窗口
 tau=zeros(1,length(sat_array{1}.tau));
-flag=sat_num;
 
-for j=1:length(sat_array)
-    if j==flag
-        continue
-    else
-        tau=tau+sat_array{j}.tau;
-    end
+flag=length(sat_array);
+if sat_num==1
+    tau=A(sat_num,flag).*sat_array{flag}.tau+A(sat_num,sat_num+1).*sat_array{sat_num+1}.tau;
+elseif sat_num==flag
+    tau=A(sat_num,sat_num-1).*sat_array{sat_num-1}.tau+A(sat_num,1).*sat_array{1}.tau;
+else
+    tau=A(sat_num,sat_num-1).*sat_array{sat_num-1}.tau+A(sat_num,sat_num+1).*sat_array{sat_num+1}.tau;
 end
 sat=sat_array{sat_num};
 sat=sat.update(x);
